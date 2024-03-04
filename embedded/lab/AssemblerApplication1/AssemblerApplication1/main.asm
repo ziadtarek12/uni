4a482b8e2f0ca8109282ed954458b208d8d20071
ziadtarek12 <zi3dtarek@gmail.com>
2024-03-04 08:18:42 +0200

    Updates

--- embedded/lab/AssemblerApplication1/AssemblerApplication1/main.asm
+++ embedded/lab/AssemblerApplication1/AssemblerApplication1/main.asm
@@ -1,4 +1,4 @@
-;
+	;
 ; AssemblerApplication1.asm
 ;
 ; Created: 23/02/2024 09:46:58
@@ -7,3 +7,10 @@
 
 
 .ORG 00 
+LDI R16, 200
+BACK: LDI R18, 100
+HERE: NOP
+DEC R18
+BRNE HERE
+DEC R16
+BRNE BACK
